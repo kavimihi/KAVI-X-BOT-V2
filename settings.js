659a799cf697f8006dd213b021815a2e0a571821
kavimihi <150218589+kavimihi@users.noreply.github.com>
2025-08-29 18:54:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~G1QniQDC#K3srB19-uMFix6mZxuyR_nAilPhaytPxbwAv8j6-BmE"
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~z1BGxKQS#FYrf-SRo3woGGkPDlj7hof4mkWS1EIaOB9Xtra3RHvU"
 global.mongodburi = process.env.MONGODB_URI || "mongodb+srv://cyberkavi011:cyberkavi011@kavi-x-bot-v2.uy91zv8.mongodb.net/safasfakfja?retryWrites=true&w=majority&appName=KAVI-X-BOT-V2"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94766577249"
